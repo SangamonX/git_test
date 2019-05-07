c1598ef791f2a1f27ed8a4bd1d0434b7f7cb7eee
crazy <crazy94xu@163.com>
2019-05-07 16:06:57 +0800

    添加至git

--- git_demo/module1/src/ClassDemo1.java
+++ git_demo/module1/src/ClassDemo1.java
@@ -1,5 +1,11 @@
 public class ClassDemo1 {
     public static void main(String[] args) {
         System.out.println("asdfg");
+        System.out.println("asdfg");
+        System.out.println("asdfg");
+        System.out.println("asdfg");
+        System.out.println("asdfg");
+        System.out.println("asdfg");
+
     }
 }
